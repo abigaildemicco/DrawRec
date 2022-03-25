a546d350feef8bea939a4cf5eba858c676c2c682
Abigail De Micco <abdemicco@gmail.com>
2022-03-25 12:47:45 +0100

    deleted useless code

--- DrawRec/ViewController.swift
+++ DrawRec/ViewController.swift
@@ -16,10 +16,8 @@ class ViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserv
     @IBOutlet weak var canvasView: PKCanvasView!
     
     @IBOutlet weak var recNum: UILabel!
-    var requests = [VNRequest]()
     
     let canvasWidth: CGFloat = 414
-    let canvasOverscrollHight: CGFloat = 454
     let context = CIContext()
     var pixelBuffer: CVPixelBuffer?
     
@@ -31,7 +29,6 @@ class ViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserv
         super.viewDidAppear(animated)
         
         canvasView.delegate = self
-        canvasView.isOpaque = true
         canvasView.drawing = drawing
         
         canvasView.alwaysBounceVertical = true
@@ -43,7 +40,6 @@ class ViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserv
         
         canvasView.becomeFirstResponder()
         
-        //setupVision()
     }
     
     override func viewDidLayoutSubviews() {
@@ -53,36 +49,9 @@ class ViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserv
         canvasView.minimumZoomScale = canvasScale
         canvasView.maximumZoomScale = canvasScale
         canvasView.zoomScale = canvasScale
-
-        updateContentSizeForDrawing()
         canvasView.contentOffset = CGPoint(x: 0, y: -canvasView.adjustedContentInset.top)
     }
     
-//    func setupVision() {
-//        // load MNIST model for the use with the Vision framework
-//        guard let visionModel = try? VNCoreMLModel(for: MNISTClassifier(configuration: MLModelConfiguration()).model) else {fatalError("can not load Vision ML model")}
-//
-//        // create a classification request and tell it to call handleClassification once its done
-//        let classificationRequest = VNCoreMLRequest(model: visionModel, completionHandler: self.handleClassification)
-//
-//        self.requests = [classificationRequest] // assigns the classificationRequest to the global requests array
-//
-//    }
-    
-//    func handleClassification (request:VNRequest, error:Error?) {
-//        guard let observations = request.results else {print("no results"); return}
-//
-//        // process the ovservations
-//        let classifications = observations
-//            .compactMap({$0 as? VNClassificationObservation}) // cast all elements to VNClassificationObservation objects
-//            .filter({$0.confidence > 0.8}) // only choose observations with a confidence of more than 80%
-//            .map({$0.identifier}) // only choose the identifier string to be placed into the classifications array
-//
-//        DispatchQueue.main.async {
-//            self.recNum.text = classifications.first // update the UI with the classification
-//        }
-//
-//    }
     
     override var prefersHomeIndicatorAutoHidden: Bool {
         return true
@@ -110,7 +79,7 @@ class ViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserv
         
         if image != nil {
                         // load our CoreML Pokedex model
-                        let model = MNISTClassifier()
+           guard let model = try? MNISTClassifier(configuration: MLModelConfiguration()) else {fatalError("can not load Vision ML model")}
             
                         let ciImage = CIImage(image: image!) //converto UIimage in CIImage
                         let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
@@ -128,45 +97,7 @@ class ViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserv
             
                         let recognizedNumb = output.classLabel
                 self.recNum.text = "\(recognizedNumb)"
-                        
-            
-//            let scaledImage = scaleImage(image: image!, toSize: CGSize(width: 28, height: 28)) // scale the image to the required size of 28x28 for better recognition results
-//
-//            let imageRequestHandler = VNImageRequestHandler(cgImage: scaledImage.cgImage!, options: [:]) // create a handler that should perform the vision request
-//
-//            do {
-//                try imageRequestHandler.perform(self.requests)
-//            }catch{
-//                print(error)
-//            }
         }
     }
-    
-    // scales any UIImage to a desired target size
-//    func scaleImage (image:UIImage, toSize size:CGSize) -> UIImage {
-//        UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
-//        image.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
-//        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-//        UIGraphicsEndImageContext()
-//        return newImage!
-//    }
-    
-    func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
-        updateContentSizeForDrawing()
-    }
-    
-    func updateContentSizeForDrawing(){
-        let drawing = canvasView.drawing
-        let contentHeight: CGFloat
-        
-        if !drawing.bounds.isNull {
-            contentHeight = max(canvasView.bounds.height, (drawing.bounds.maxY + self.canvasOverscrollHight) * canvasView.zoomScale)
-        } else {
-            contentHeight = canvasView.bounds.height
-        }
-        
-        canvasView.contentSize = CGSize (width: canvasWidth * canvasView.zoomScale, height: contentHeight)
-    }
-    
 }
 
